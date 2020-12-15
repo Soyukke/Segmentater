635da8f18d3085e49c83eda4ea154717eb4d0876
Soyukke <absoexrd@gmail.com>
2020-12-15 22:51:47 +0900

    add loss

--- src/model.jl
+++ src/model.jl
@@ -9,11 +9,23 @@ struct SegmentationModel
 end
 
 function SegmentationModel()
+    k₁ = (32, 32); s₁ = (16, 16);
     encoder = Chain(
-        Conv((128, 128), 3 => 2, relu, stride=(128, 128))
+        Conv(k₁, 3 => 3, relu, stride=s₁)
     )
     decoder = Chain(
-        ConvTranspose((128, 128), 2 => 3, relu, stride=(128, 128))
+        ConvTranspose(k₁, 3 => 3, relu, stride=s₁)
     )
     return SegmentationModel(encoder, decoder)
+end
+
+function (m::SegmentationModel)(x::Array)
+    z = m.encoder(x)
+    y = m.decoder(z)
+    return y
+end
+
+function lossfunction(model)
+    loss(x, y) = Flux.Losses.mse(model(x), y)
+    return loss
 end
